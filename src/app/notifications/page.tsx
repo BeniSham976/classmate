c76adfcc140a306a12960bf41a6b9a4cbf06958e
Beni Shamavu <d00253976@student.dkit.ie>
2025-09-03 01:33:35 +0100

    Created Notifications folder, with notifications webpage and action scripts

---
+++ src/app/notifications/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { getNotifications, markNotificationsAsRead } from "@/actions/notification.action";
+import { NotificationsSkeleton } from "@/components/NotificationSkeleton";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+type Notifications = Awaited<ReturnType<typeof getNotifications>>
+type Notification = Notifications[number]
+
+function NotificationsPage() {
+    const [notifications, setNotifications] = useState<Notification[]>();
+    const [isLoading, setIsLoading] = useState(true);
+
+
+    useEffect(() => {
+        const fetchNotifications = async () => {
+            try {
+                const data = await getNotifications();
+                setNotifications(data);
+
+                const unreadIds = data.filter(n => !n.read).map(n => n.id);
+                if(unreadIds.length > 0) await markNotificationsAsRead(unreadIds);
+            } catch (error) {
+                toast.error("Fetch notifications failed");
+
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        fetchNotifications()
+    },[])
+
+    if(isLoading) return <NotificationsSkeleton /> 
+    
+    return <div>NotificationsPage</div>;
+}
+export default NotificationsPage;
